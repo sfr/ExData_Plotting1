19ead1e12c08097d831c54c7315a2e3acdafcb4a
sfr <juraj.sofranko@gmail.com>
2015-02-05 14:32:58 -0500

    bug fixes

--- src/download.data.R
+++ src/download.data.R
@@ -1,18 +1,20 @@
 ## file downloads and uzips the dataset, unless it is already unzipped or downloaded.
 
-file.name <- unlist(strsplit(URLdecode(url), '/', fixed=T))
-file.name <- file.name[length(file.name)]
-zip.file  <- getPath(DIR.DATA, file.name)
-txt.file  <- sub('.zip', '.txt', zip.file, fixed=T)
+if (!file.exists(data.subset.file)) {
+    file.name <- unlist(strsplit(URLdecode(url), '/', fixed=T))
+    file.name <- file.name[length(file.name)]
+    zip.file  <- getPath(DIR.DATA, file.name)
+    txt.file  <- sub('.zip', '.txt', zip.file, fixed=T)
 
-if (!file.exists(txt.file)) {
-    if (!file.exists(zip.file)) {
-        print('Downloading dataset...')
-        download.file(url, file.path)
+    if (!file.exists(txt.file)) {
+        if (!file.exists(zip.file)) {
+            print('Downloading dataset...')
+            download.file(url, zip.file)
+        }
+
+        print('Decompressing dataset... (slooooow)')
+        unzip(zip.file, exdir=DIR.DATA)
     }
 
-    print('Decompressing dataset...')
-    unzip(zip.file, exdir=DIR.DATA)
+    rm(file.name, zip.file, url)
 }
-
-rm(file.name, zip.file, url)
--- src/load.subset.data.R
+++ src/load.subset.data.R
@@ -5,7 +5,7 @@ if (!file.exists(data.subset.file)) {
     # load data
     print('Building data subset...')
     data <- read.csv(txt.file, sep=';', header=T, na.strings='?')
-
+    rm(txt.file)
     data$Date <- as.Date(data$Date, '%d/%m/%Y')
 
     # subset to 2007-02-01 and 2007-02-02
@@ -15,10 +15,11 @@ if (!file.exists(data.subset.file)) {
     data$datetime <- as.POSIXct(paste(data$Date, data$Time))
     data <- data[ , !(names(data) %in% c('Date', 'Time'))]
     row.names(data) <- NULL
+
     save(data, file=data.subset.file, compress='bzip2')
 } else {
     print('Loading data subset...')
     load(data.subset.file)
 }
 
-rm(data.subset.file, txt.file)
+rm(data.subset.file)
